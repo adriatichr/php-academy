514e03cd4ed0ca898c55f6a8275698aec05361fd
Danko Lucic <dlucic33@gmail.com>
2016-12-07 11:26:32 +0100

    dodao kratku uputu za korištenje unutar index.php file-a

--- example/web/index.php
+++ example/web/index.php
@@ -1,1 +1,10 @@
 <?php
+
+// Ovaj file mora biti uključen da bi mogli koristiti autoloading. Prije korištenja, spojiti se na PuTTY i unutar example
+// direktorija pokrenuti naredbu "composer install"
+require_once __DIR__ . '/../app/bootstrap.php';
+
+use Adriatic\PHPAkademija\OOPIntro\Car;	// Car klasa se nalazi u src/OOPIntro/Car.php
+
+$myCar = new Car('silver');
+echo $myCar->getCurrentSpeed();
