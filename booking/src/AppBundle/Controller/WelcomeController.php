4f479f1acb4340d0b8b355399d10de27dba0c52e
Danko Lucic <dlucic33@gmail.com>
2016-12-09 01:02:42 +0100

    dodao booking sadržaj današnjeg predavanja

---
+++ booking/src/AppBundle/Controller/WelcomeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class WelcomeController extends Controller
+{
+    /**
+     * @Route("/", name="AppBundle_Welcome_homepage")
+     */
+    public function homepageAction(Request $request)
+    {
+        return $this->render('AppBundle:Welcome:homepage.html.twig');
+    }
+
+    /**
+     * @Route("/about", name="AppBundle_Welcome_about")
+     */
+    public function aboutAction()
+    {
+        return $this->render('AppBundle:Welcome:about.html.twig');
+    }
+}
