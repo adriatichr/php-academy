54d69c3299df9498e6fe86c9026cc8be2e948543
Danko Lucic <dlucic33@gmail.com>
2016-12-10 21:27:41 +0100

    dodao primjere za closure scope

--- example/test/OOPIntro/ClosuresTest.php
+++ example/test/OOPIntro/ClosuresTest.php
@@ -4,10 +4,41 @@ use PHPUnit\Framework\TestCase;
 
 class ClosuresTest extends TestCase
 {
+    private $foo = 'bar';
+
     /** @test */
     public function sumClosure()
     {
         $this->expectOutputString('9');
         require_once __DIR__ . '/../../src/OOPIntro/Closures.php';
+    }
+
+    /** @test */
+    public function closureScope()
+    {
+        $closure = $this->getClosure();
+        $this->assertEquals('bar', ($closure)());
+    }
+
+    /** @test */
+    public function closureAutomaticallyBindsThisPseudoVariableWhenCreatedInObjectContext()
+    {
+        $closure = $this->getClosureWithThis();
+        $this->assertEquals('bar', ($closure)());
+    }
+
+    private function getClosure()
+    {
+        $foo = 'bar';
+        return function() use ($foo) {
+            return $foo;
+        };
+    }
+
+    private function getClosureWithThis()
+    {
+        return function() {
+            return $this->foo;
+        };
     }
 }
