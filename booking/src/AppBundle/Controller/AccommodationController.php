7f5e89feb161239a4e75b5b0b42ecf087da6fb10
Danko Lucic <dlucic33@gmail.com>
2016-12-13 01:07:07 +0100

    dodan sadržaj predavanja od ponedjeljka

--- booking/src/AppBundle/Controller/AccommodationController.php
+++ booking/src/AppBundle/Controller/AccommodationController.php
@@ -13,8 +13,32 @@ class AccommodationController extends Controller
      */
     public function accommodationAction($accommodationId)
     {
-        return $this->render('AppBundle:Accommodation:accommodation.html.twig', [
-            'id' => $accommodationId
+        $queryBuilder = $this->get('database_connection')->createQueryBuilder();
+        $statement = $queryBuilder
+            ->select('a.*', 'p.name AS place_name')
+            ->from('accommodation', 'a')
+            ->join('a', 'place', 'p', 'p.id = a.place_id')
+            ->where('a.id = ?')
+            ->setParameter(0, $accommodationId)
+            ->execute();
+        $accommodation = $statement->fetch();
+        if(!$accommodation)
+            throw $this->createNotFoundException(sprintf('Accommodation with id "%s" not found.', $accommodationId));
+
+        return $this->render('AppBundle:Accommodation:accommodation.html.twig', ['accommodation' => $accommodation]);
+    }
+
+    /**
+     * @Route("/accommodation", name="AppBundle_Accommodation_accommodationList")
+     */
+    public function accommodationListAction()
+    {
+        $conn = $this->get('database_connection');
+        $statement = $conn->query('SELECT * FROM accommodation');
+
+        return $this->render('AppBundle:Accommodation:accommodationList.html.twig', [
+            'accommodations' => $statement->fetchAll()
         ]);
     }
+
 }
