adab79f15af68b21c44b3fedf56c736dd9388163
Danko Lučić <danko.lucic@adriatic.hr>
2016-12-16 15:27:08 +0100

    dodao stranicu za primjer PHP closure-a i komentar u klasi koji bolje objašnjava način korištenja

--- example/src/OOPIntro/ClosureExample/ArticleRepository.php
+++ example/src/OOPIntro/ClosureExample/ArticleRepository.php
@@ -8,6 +8,12 @@ class ArticleRepository
 
     public function getArticle()
     {
+        /**
+         * Umjesto da tagove dohvaćamo odmah i šaljemo ih Article klasi, umjesto toga šaljemo closure koji će Article
+         * izvršiti baš kada se na njemu pozove metoda getTags(). Drugim riječima, umjesto da smo odmah dohvatili tagove
+         * i poslali ih u Article, mi Article klasi šaljemo "upute" kako da sam dohvati tagove. Article onda može ako
+         * želi odgoditi dohvaćanje tagova za trenutak kada se točno pozove getTags() metoda.
+         */
         return new Article(function () {
             return $this->getTags();
         });
---
+++ example/web/OOPExamples/Closure.php
@@ -0,0 +1,23 @@
+<?php
+
+// http://phpacademy.example.{inicijali}/web/OOPExamples/Closure.php
+require_once __DIR__ . '/../../app/bootstrap.php';
+
+use Adriatic\PHPAkademija\OOPIntro\ClosureExample\ArticleRepository;
+
+?>
+
+<p>Primjer korištenja closure-a za lazy loading:</p>
+
+<?php
+$articleRepository = new ArticleRepository();
+$article = $articleRepository->getArticle();
+
+echo '<br />';
+echo ArticleRepository::$getTagsMethodCalled ? 'Tagovi dohvaćeni iz baze' : 'Tagovi još nisu dohvaćeni iz baze';
+
+$tags = $article->getTags();
+echo '<br />';
+echo 'Pozvana getTags() metoda na klasi Article';
+echo '<br />';
+echo ArticleRepository::$getTagsMethodCalled ? 'Tagovi dohvaćeni iz baze' : 'Tagovi još nisu dohvaćeni iz baze';
