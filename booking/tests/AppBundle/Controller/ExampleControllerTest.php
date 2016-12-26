390d973d506cf918389d1e1c54ade54ada6e6fc6
Danko Lucic <dlucic33@gmail.com>
2016-12-26 17:47:20 +0100

    refaktorirao i proširio smoke testove za neke ExampleController akcije

--- booking/tests/AppBundle/Controller/ExampleControllerTest.php
+++ booking/tests/AppBundle/Controller/ExampleControllerTest.php
@@ -6,17 +6,40 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ExampleControllerTest extends WebTestCase
 {
+    private $client;
+
+    public function setUp()
+    {
+        $this->client = static::createClient();
+    }
+
     public function testAccommodationDbalQueryBuilder()
     {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/example/accommodation/1/dbal-query-builder');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $crawler = $this->client->request('GET', '/example/accommodation/1/dbal-query-builder');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
     }
 
     public function testStoringAccommodationAction()
     {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/example/accommodation/insert');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $crawler = $this->client->request('GET', '/example/accommodation/insert');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testExpirationCacheAction()
+    {
+        $crawler = $this->client->request('GET', '/example/cache/expiration/super-slow-page');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testETagValidationCacheAction()
+    {
+        $crawler = $this->client->request('GET', '/example/cache/validation/etag');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testLastModifiedValidationCacheAction()
+    {
+        $crawler = $this->client->request('GET', '/example/cache/validation/last-modified/accommodation/1');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
     }
 }
