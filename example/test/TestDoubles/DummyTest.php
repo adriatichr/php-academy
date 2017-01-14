90ba63ddcb03f890ffe9d6c54bf0114f39ae9e70
Danko Lucic <dlucic33@gmail.com>
2017-01-15 00:27:10 +0100

    dodao primjer korištenja Dummy test double objekta

---
+++ example/test/TestDoubles/DummyTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Adriatic\PHPAkademija\Test\SolidPrinciples\DependencyInversionPrinciple;
+
+use Adriatic\PHPAkademija\SolidPrinciples\DependencyInversionPrinciple\DIPSolution\ReservationServiceImpl;
+use Adriatic\PHPAkademija\SolidPrinciples\DependencyInversionPrinciple\DIPSolution\ProviderNotifier;
+use PHPUnit\Framework\TestCase;
+
+class DummyTest extends TestCase
+{
+    /** @test */
+    public function canAcceptReservation()
+    {
+        // ProviderNotifier nam je potreban samo za bookAccommodation metodu, stoga za testiranje acceptReservation()
+        // metode servisu šaljemo DummyProviderNotifier
+        $reservationService = new ReservationServiceImpl(new DummyProviderNotifier());
+        $this->assertEquals('Prihvaćamo zahtjev za rezervacijom "15" smještaja za korisnika',
+            $reservationService->acceptReservation(15));
+    }
+}
+
+
+class DummyProviderNotifier implements ProviderNotifier
+{
+    public function askForConfirmation(int $accommodationId) {}
+}
